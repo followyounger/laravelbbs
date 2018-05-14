46ef78efed920ad4c4a16e65e3a6a342017668b3
vagrant <vagrant@homestead>
2018-05-14 05:11:28 +0000

    话题页面

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -16,7 +16,8 @@ class TopicsController extends Controller
 
 	public function index()
 	{
-		$topics = Topic::paginate();
+		$topics = Topic::with('user','category')->paginate(30);
+//		dd($topics);
 		return view('topics.index', compact('topics'));
 	}
 
