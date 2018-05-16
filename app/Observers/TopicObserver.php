70de876ac92022e4e77e4bf69016e052b7f5a2e5
vagrant <vagrant@homestead>
2018-05-16 09:32:58 +0000

    添加队列

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -4,6 +4,8 @@ namespace App\Observers;
 
 use App\Models\Topic;
 use App\Handlers\SlugTranslateHandler;
+use App\Jobs\TranslateSlug;
+
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
@@ -23,10 +25,17 @@ class TopicObserver
 
         $topic->excerpt = make_excerpt($topic->body);
 
+    }
+
+    public function saved(Topic $topic)
+    {
         // 如果slug字段无内容，即  使用翻译器对title进行编译
         if (! $topic->slug)
         {
-            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+//            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+
+            //推送任务到队列
+            dispatch(new TranslateSlug($topic));
         }
     }
 
